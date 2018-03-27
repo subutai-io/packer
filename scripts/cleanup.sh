f69619fa194f4b46a2d8439f8c440059865027f6
debian <debian@debian>
2018-03-27 10:12:05 -0400

    disk interface changed and interface ens5 added

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -24,3 +24,6 @@ fi
 echo "Replacing /etc/apt/sources.list with standard sources"
 cp /tmp/sources.list /etc/apt/sources.list
 apt-get update
+
+echo "auto ens5" >> /etc/network/interfaces
+echo "iface ens5 inet dhcp" >> /etc/network/interfaces
