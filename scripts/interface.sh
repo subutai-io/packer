ac98352d8b04543a83bc3b6a4a75bb4b1a489e91
jadilet <adilet.571@gmail.com>
2018-04-11 19:02:24 +0600

    removed vmware hostonly interface

--- scripts/interface.sh
+++ scripts/interface.sh
@@ -4,9 +4,9 @@ echo "auto eth1" >> /etc/network/interfaces
 echo "iface eth1 inet dhcp" >> /etc/network/interfaces
 echo "  port-up route del default dev $IFACE || true" >> /etc/network/interfaces
 
-echo "auto eth2" >> /etc/network/interfaces
-echo "iface eth2 inet dhcp" >> /etc/network/interfaces
-echo "  port-up route del default dev $IFACE || true" >> /etc/network/interfaces
+#echo "auto eth2" >> /etc/network/interfaces
+#echo "iface eth2 inet dhcp" >> /etc/network/interfaces
+#echo "  port-up route del default dev $IFACE || true" >> /etc/network/interfaces
 
 
 echo Changed interface configuration
