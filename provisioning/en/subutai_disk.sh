6acfc08745eed796618bc8ceaa33f9b739746d6b
jadilet <adilet.571@gmail.com>
2018-06-01 09:59:37 +0600

    removed third interface hostonly and changed disk message

--- provisioning/en/subutai_disk.sh
+++ provisioning/en/subutai_disk.sh
@@ -6,10 +6,10 @@ result=`lsblk -o SIZE,NAME --bytes | grep "$((1073741824*(DISK_SIZE+2)))" | tail
 disk_name=`expr match "$result" '.*\([s][d][b-z]*\)'`
 
 if [ -z "$disk_name" ]; then
-  echo ' ==> [ERROR] Not found disk'
+  echo 'Not found disk'
 else
   disk="/dev/${disk_name}"
-  echo ' ==> [OK] Added new virtual disk '$disk
+  echo 'Added new virtual disk '$disk
   sudo zpool set autoexpand=on subutai
   sudo zpool add -f subutai "$disk"
 fi
