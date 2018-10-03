06b76a649ccca64f0d85dbdc346c24f2c3b4d850
jadilet <adilet.571@gmail.com>
2018-10-03 17:42:14 +0600

    added set locale

--- provisioning/en/peer_cmd.sh
+++ provisioning/en/peer_cmd.sh
@@ -33,7 +33,11 @@ fi
 
 errcode=0
 if [ -n "$(/usr/bin/$CMD list containers -n management | grep management)" ]; then
-  localedef -i en_US -f UTF-8 en_US.UTF-8
+  export LANGUAGE=en_US.UTF-8
+  export LANG=en_US.UTF-8
+  export LC_ALL=en_US.UTF-8
+  locale-gen en_US.UTF-8
+  dpkg-reconfigure locales
   echo "Management seems to already be installed. Checking for upgrades..."
   /usr/bin/$CMD update management
   exit 0
